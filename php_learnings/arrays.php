4dfb51ea4c5fdb9f06a140cfa936ce643aec5927
SANJAI-909 <sanjaisk83@gmail.com>
2024-06-27 17:40:14 +0000

    starting..

---
+++ php_learnings/arrays.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays learningt</title>
+</head>
+<body>
+    <h1>ARRAYS</h1>
+    <?
+
+    $arr=[1,2,3,4,5];
+
+    foreach($arr as $i){
+       // echo "$i\n";
+       printf("%d <hr> ",$i);
+    }
+    ?>
+    
+</body>
+</html>
